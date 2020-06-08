ba51472f9099de933a0c4d09d6f024581e6894ad
Hu Rui <hr101191@gmail.com>
2020-06-08 14:52:43 +0800

    working cluster template

--- config-store-example/infinispan/infinispan-configstore-client/src/main/java/com/hurui/verticles/MainVerticle.java
+++ config-store-example/infinispan/infinispan-configstore-client/src/main/java/com/hurui/verticles/MainVerticle.java
@@ -1,11 +1,96 @@
 package com.hurui.verticles;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.ext.cluster.infinispan.InfinispanClusterManager;
 
 public class MainVerticle extends AbstractVerticle {
 	
+	private static final Logger logger = LoggerFactory.getLogger(new Object() { }.getClass().getEnclosingClass());
+	private JsonObject localConfig = new JsonObject();
+	
 	@Override
 	public void start() {
+		logger.info("Initializing Main Verticle");
 		
+		//get the initial config
+		if(vertx.getOrCreateContext().config().isEmpty()) {
+			//build config store from src/main/resources and read the default config
+			logger.info("No --conf overload detected from CLI, loading default-config.json from src/main/resoruces");
+			ConfigStoreOptions fileConfigStoreOptions = new ConfigStoreOptions()
+					.setType("file")
+					.setOptional(true)
+					.setConfig(new JsonObject()
+							.put("path", "src/main/resources/default-config.json"));
+			
+			ConfigRetrieverOptions configRetrieverOptions = new ConfigRetrieverOptions()
+					.setScanPeriod(2000000)
+					.addStore(fileConfigStoreOptions);
+			
+			ConfigRetriever configRetriever = ConfigRetriever.create(vertx, configRetrieverOptions);
+			
+			configRetriever.getConfig(completionHandler -> {
+				if(completionHandler.succeeded()) {
+					localConfig = completionHandler.result(); //Do something with the default config if required
+					logger.info("Default config loaded successfully: " + localConfig);
+				} else {
+					logger.error("Failed to read default-config.json from src/main/resoruces, stacktrace: ", completionHandler.cause());
+					logger.warn("Empty JsonObject used, some components may not load properly!");
+				}
+			});
+		} else {
+			//react to the --conf overload from command line and set the config based on the overload path
+			localConfig = vertx.getOrCreateContext().config(); //Note that no error message will be thrown here, please ensure input is correct!!
+			logger.info("Initial configuration from external context: " + localConfig);
+		}	
+		
+		VertxOptions vertxOptions = new VertxOptions();
+		Future<ClusterManager> clusterManagerFuture = buildClusterManager();
+		clusterManagerFuture.onComplete(handler -> {
+			if(handler.succeeded()) {
+				vertxOptions.setClusterManager(handler.result());
+			} else {
+				logger.error("Failed to created an instance of Infinispan Cluster Manager, stacktrace: " , handler.cause());
+				logger.info("Creating a new instance of Infinispan Cluster Manager using the default settings...");
+				vertxOptions.setClusterManager(new InfinispanClusterManager());
+			}
+		});
+		
+		Vertx.clusteredVertx(vertxOptions, resultHandler -> {
+			if(resultHandler.succeeded()) {
+				logger.info("Obtained an instance of clustered Vertx successfully");
+			}
+		});
+	}
+	
+	private Future<ClusterManager> buildClusterManager() {
+		Promise<ClusterManager> promise = Promise.promise();
+		vertx.<ClusterManager>executeBlocking(blockingCodeHandler -> {
+			try {
+				ClusterManager clusterManager = new InfinispanClusterManager();
+				promise.complete(clusterManager);
+			} catch(Exception ex) {
+				promise.fail(ex);
+			}			
+		}, resultHandler -> {
+			if(resultHandler.succeeded()) {
+				promise.complete(resultHandler.result());
+			} else {
+				promise.fail(resultHandler.cause());
+			}
+		});
+		
+		return promise.future();
 	}
 }
