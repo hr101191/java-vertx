78b5969884cd9d8aa16dfbeb87b8dddb5c59326b
Hu Rui <hr101191@gmail.com>
2020-07-28 22:17:56 +0800

    Override vertx option using custom launcher

--- micrometer-metrics-example/prometheus/prometheus-metrics-service/src/main/java/com/hurui/micrometermetrics/prometheusmetricsservice/PrometheusMetricsServiceApplication.java
+++ micrometer-metrics-example/prometheus/prometheus-metrics-service/src/main/java/com/hurui/micrometermetrics/prometheusmetricsservice/PrometheusMetricsServiceApplication.java
@@ -6,24 +6,44 @@ import org.slf4j.LoggerFactory;
 import com.hurui.micrometermetrics.verticles.MainVerticle;
 
 import io.vertx.core.Launcher;
+import io.vertx.core.VertxOptions;
+import io.vertx.micrometer.MicrometerMetricsOptions;
+import io.vertx.micrometer.VertxPrometheusOptions;
 
-public class PrometheusMetricsServiceApplication {
+public class PrometheusMetricsServiceApplication extends Launcher {
 
 	private static final Logger logger = LoggerFactory.getLogger(new Object() { }.getClass().getEnclosingClass());
 	
 	public static void main(String[] args) {
+		
 		logger.info("Initializing Vertx Command Launcher...");
-		String[] newArgs = new String[args.length + 1];
-	    newArgs[0] = MainVerticle.class.getName();
-	    System.arraycopy(args, 0, newArgs, 1, args.length);
+		String[] newArgs = new String[args.length + 2];
+		newArgs[0] = "run";
+	    newArgs[1] = MainVerticle.class.getName();
+	    System.arraycopy(args, 0, newArgs, 2, args.length);
 	    
 	    logger.info("Executing the program with the following command line arguements:");	    
 	    for(String str : newArgs) {
 	    	logger.info("args: " + str);
 	    }
 	    logger.info("Deploying Main Verticle...");
-	    
-		Launcher.executeCommand("run", newArgs);
 
+		new PrometheusMetricsServiceApplication().dispatch(newArgs);
 	}
+	
+    @Override
+    public void beforeStartingVertx(VertxOptions options) {
+        // Customize the options
+       	logger.info("Configuring Prometheus options...");
+		// Deploy with embedded server: prometheus metrics will be automatically exposed,
+	    // independently from any other HTTP server defined
+	    MicrometerMetricsOptions micrometerMetricsOptions = new MicrometerMetricsOptions()
+		    		.setPrometheusOptions(new VertxPrometheusOptions()
+		    		//.setStartEmbeddedServer(true)
+		    		//.setEmbeddedServerOptions(new HttpServerOptions().setPort(9090))
+		    		.setEnabled(true)
+	    		)
+	    		.setEnabled(true);
+	    options.setMetricsOptions(micrometerMetricsOptions);
+    }
 }
--- micrometer-metrics-example/prometheus/prometheus-metrics-service/src/main/java/com/hurui/micrometermetrics/verticles/MainVerticle.java
+++ micrometer-metrics-example/prometheus/prometheus-metrics-service/src/main/java/com/hurui/micrometermetrics/verticles/MainVerticle.java
@@ -18,8 +18,8 @@ public class MainVerticle extends AbstractVerticle {
 	
 	@Override
 	public Completable rxStart() {
+		logger.info("Json Config from context: " +  vertx.getOrCreateContext().config().toString());
 		logger.info("Deploying Verticles...");
-		loadPrometheusOptions();
 		return deployVerticles()
 				.doOnComplete(() -> {
 					logger.info("Successfully deployed all verticles...");
@@ -37,27 +37,6 @@ public class MainVerticle extends AbstractVerticle {
 				.doOnError(onError -> {
 					logger.error("Failed to deploy Http Server Verticle...");
 				});
-	}
-
-	private void loadPrometheusOptions() {
-		// Deploy with embedded server: prometheus metrics will be automatically exposed,
-	    // independently from any other HTTP server defined
-	    MicrometerMetricsOptions micrometerMetricsOptions = new MicrometerMetricsOptions()
-		    		.setPrometheusOptions(new VertxPrometheusOptions()
-		    		//.setStartEmbeddedServer(true)
-		    		//.setEmbeddedServerOptions(new HttpServerOptions().setPort(9090))
-		    		.setEnabled(true)
-	    		)
-	    		.setEnabled(true);		 
-	    
-	    /* (non-Javadoc)
-	     * Create a new instance of Vertx and set the VertxOptions. 
-	     * Then use this instance of Vertx to override the existing Vertx from this verticle and deploy the other verticles
-	     * 
-	     * You will see the warning below in the log because Vertx.vertx() always creates a new instance of Vertx:
-	     * WARNING: You're already on a Vert.x context, are you sure you want to create a new Vertx instance? 
-	     */
-	     vertx = Vertx.vertx(new VertxOptions().setMetricsOptions(micrometerMetricsOptions));
 	}
 	
 	private Single<String> deployHttpServerVerticle() {
